6f5e74d1317b84edc0732b09323a27136c73966d
timaevV <timaevvladislav@gmail.com>
2023-10-17 02:12:45 +0600

    add:user controller

---
+++ server/controllers/user-controller.js
@@ -0,0 +1,54 @@
+class UserController {
+    async registration(req, res) {
+        try {
+            const {email, password} = req.body
+
+        } catch (e) {
+
+        }
+    }
+
+    async login(req, res) {
+        try {
+            const {email, password} = req.body
+
+        } catch (e) {
+
+        }
+    }
+
+    async logout(req, res) {
+        try {
+            const {email, password} = req.body
+
+        } catch (e) {
+
+        }
+    }
+
+    async activate(req, res) {
+        try {
+            const {email, password} = req.body
+
+        } catch (e) {
+
+        }
+    }
+
+    async refresh(req, res) {
+        try {
+            const {email, password} = req.body
+
+        } catch (e) {
+
+        }
+    }
+
+    async getUsers(req, res) {
+        try {
+
+        } catch (e) {
+
+        }
+    }
+}
