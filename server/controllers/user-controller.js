f11d5772a615c2e3bde93409199ce24bbbe2308f
timaevV <timaevvladislav@gmail.com>
2023-10-18 01:09:21 +0600

    add:register logic

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,7 +1,19 @@
+const User = require("../models/user")
+const bcrypt = require("bcrypt")
+const uuid = require("uuid")
+
 class UserController {
     async registration(req, res) {
         try {
             const {email, password} = req.body
+            const candidate = await User.findOne({email})
+
+            if (candidate) {
+                return res.status(400).json({message: `User with email ${email} already exists`})
+            }
+
+            const hashPassword = bcrypt.hashSync(password, 7)
+            const user = await User.create({email, password: hashPassword, activation: uuid.v4()})
             res.json({message: "User was created"})
         } catch (e) {
 
