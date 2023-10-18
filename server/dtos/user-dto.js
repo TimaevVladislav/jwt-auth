b000ca76537b8d05fece64d1e36d7bc77a4025f8
timaevV <timaevvladislav@gmail.com>
2023-10-18 22:45:03 +0600

    add:user dto

---
+++ server/dtos/user-dto.js
@@ -0,0 +1,12 @@
+
+module.exports = class UserDto {
+    email
+    isActivated
+    id
+
+    constructor(model) {
+        this.email = model.email
+        this.isActivated = model.isActivated
+        this.id = model._id
+    }
+}
