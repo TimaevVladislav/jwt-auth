94f50c2e28be88a0fabd923ba1055a04a8be8905
timaevV <timaevvladislav@gmail.com>
2023-10-18 23:22:17 +0600

    add:send email logic

--- server/services/email-service.js
+++ server/services/email-service.js
@@ -1,8 +1,30 @@
+const email = require("nodemailer")
 
 class EmailService {
+
+    constructor() {
+        this.transporter = email.createTransport({
+            auth: {user: process.env.SMTP_USER, pass: process.env.SMTP_PASSWORD},
+            host: process.env.SMTP_HOST,
+            port: process.env.SMTP_PORT,
+            secure: false
+        })
+    }
+
     async authEmail(email, activation) {
-
-
+        await this.transporter.sendMail({
+            from: process.env.SMTP_USER,
+            to: email,
+            subject: `Account activation on ${process.env.API_URL}`,
+            text: "",
+            html:
+                `
+                <div>
+                    <h1>Click on link to activate your account</h1>
+                    <a href="${activation}">${activation}</a>
+                </div>
+                `
+        })
     }
 }
 
