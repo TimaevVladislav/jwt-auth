645b8f14918a8f5307f47da6f96f6ed0e36c1db6
timaevV <timaevvladislav@gmail.com>
2023-10-20 00:17:09 +0600

    add:interceptors api

--- client/src/api.ts
+++ client/src/api.ts
@@ -1,6 +1,14 @@
 import axios from "axios"
 
-export default axios.create({
+const api = axios.create({
     withCredentials: true,
     baseURL: process.env.REACT_APP_BASE_URL
 })
+
+
+api.interceptors.request.use((config) => {
+  config.headers.Authorization = `Bearer ${localStorage.getItem("token")}`
+  return config
+})
+
+export default api
