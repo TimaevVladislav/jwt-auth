ddf32d7c4656a4a3f3b8cc38afd3bcc330bcc34d
timaevV <timaevvladislav@gmail.com>
2023-10-20 23:15:51 +0600

    add:mobx store

---
+++ client/src/store/index.ts
@@ -0,0 +1,54 @@
+import { makeObservable} from "mobx"
+import {IUser} from "../interfaces/user.interface"
+import {IAuthPayload} from "../interfaces/auth.interface"
+import AuthService from "../services/AuthService";
+
+export default class Store {
+    user = {} as IUser
+    isAuth = false
+
+    constructor() {
+        makeObservable(this)
+    }
+
+    setAuth(bool: boolean) {
+        this.isAuth = bool
+    }
+
+    setUser(user: IUser) {
+        this.user = user
+    }
+
+    async login(payload: IAuthPayload) {
+        try {
+            const response = await AuthService.login(payload)
+            localStorage.setItem("token", response.data.accessToken)
+            this.setAuth(true)
+            this.setUser(response.data.user)
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    async registration(payload: IAuthPayload) {
+        try {
+            const response = await AuthService.registration(payload)
+            localStorage.setItem("token", response.data.accessToken)
+            this.setAuth(true)
+            this.setUser(response.data.user)
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    async logout() {
+        try {
+            const response = await AuthService.logout()
+            localStorage.removeItem("token")
+            this.setAuth(false)
+            this.setUser({} as IUser)
+        } catch (e) {
+            console.log(e)
+        }
+    }
+}
