bb342c788b5bd594f28dd051fb0b550d3e2cf1c5
timaevV <timaevvladislav@gmail.com>
2023-10-21 02:42:59 +0600

    fix:cors

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cookie())
-app.use(cors())
+app.use(cors({credentials: true, origin: process.env.CLIENT_URL}))
 app.use("/api", routes)
 app.use(errorMiddleware)
 
